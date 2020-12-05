33a57195264b7159aa8e385192b17ec0e121acc1
Illya Artemchuk <ilyalinkis@gmail.com>
2020-12-05 14:25:36 -0800

    working on list of lyrics

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
+import { Link } from "react-router";
 import fetchSong from "../queries/fetchSong";
+import LyricCreate from "./LyricCreate";
+import LyricList from "./LyricList";
 
 class SongDetail extends Component {
   render() {
-    console.log(this.props);
+    const { song } = this.props.data;
+
+    if (!song) {
+      return (
+        <div>
+          <h3> Loading </h3>
+        </div>
+      );
+    }
     return (
       <div>
-        <h3>Song Detail</h3>
+        <Link to="/">Back</Link>
+        <h3>{song.title}</h3>
+        <LyricList />
+        <LyricCreate songId={this.props.params.id} />
       </div>
     );
   }
